c49fb2cc9f03d6cfe5297d148b6edc610baed904
Krishna K Mishra <Krishna.Mishra@mea.ey.net>
2018-02-08 16:08:54 +0530

    part one of exercise completed to query sharepoint list

--- app/tomato-exercise-three/mockRest.service.js
+++ app/tomato-exercise-three/mockRest.service.js
@@ -10,7 +10,8 @@
 	function mockRestService($http, $q) {
 
 		var service = {
-            getListItems: getListItems
+			getListItems: getListItems,
+			getAllItems: getAllItems
 		};
 
 		return service;
@@ -26,6 +27,57 @@
             });
             return dfd.promise;
 		}
+
+		function getAllItems(listTitle, params) {
+            var filters = [];
+			
+			//setting an int value to maximum no. of rows returned from query
+			if (params && params.top) filters.push("$top=" + params.top);
+			
+			//setting column name/names to sort the returned rows from query
+            if (params && params.orderby) filters.push("$orderby=" + params.orderby);
+
+            var queryObjects = {
+                filterString: "",
+                expandString: "",
+                selectString: ""
+            }
+
+			//condition to filter the returned rows from query based column values conditions.
+            if (params && params.filter) {
+                queryObjects.filterString = params.filter;
+            }
+
+			//in case of lookup or people picker columns to get details of other fields.
+            if (params && params.expand) {
+                queryObjects.expandString = params.expand;
+            }
+			
+			//to sepcify all columns you need from list
+            if (params && params.select) {
+                queryObjects.selectString = params.select;
+            }
+
+            filters.push("$select=" + queryObjects.selectString);
+            filters.push("$filter=" + queryObjects.filterString);
+            filters.push("$expand=" + queryObjects.expandString);
+
+            var query = "?" + filters.join("&");
+            return (
+                $http({
+                    method: "GET",
+                    url: baseUrl + "/_api/web/lists/GetByTitle('" + listTitle + "')/Items" + query,
+                    headers: {
+                        "accept": "application/json; odata=verbose"
+                    }
+                }).then(function(response) {
+                    if (!response.data) {
+                        return;
+                    }
+                    return response.data.d.results;
+                })
+            );
+        }
 
 	}
 })();
@@ -45,6 +97,8 @@
 		mockRestService.getListItems('WorkPlan', queryParams).then(function(response) {
 			return response;
 		})
+
+		
 	}
 
 	ANTICIPATED DATA FORMAT: 
--- app/tomato-exercise-three/tomato-exercise-three.controller.js
+++ app/tomato-exercise-three/tomato-exercise-three.controller.js
@@ -6,32 +6,56 @@
         .controller('TomatoExerciseThreeController', TomatoExerciseThreeController);
     
     TomatoExerciseThreeController.$inject = [
-        '$scope', 'restService', '$q'
+        '$scope', 'restService', '$q', 'mockRestService'
     ];
 
-    function TomatoExerciseThreeController($scope, restService, $q) {
+    function TomatoExerciseThreeController($scope, restService, $q, mockRestService) {
         var vm = this;
+
+        vm.HighRiskAndIssues = [];
+
+        //used all internal names.
+        vm.listTitleRisksandIssues = "Risks and Issues";
+        vm.paramsHighRiskAndIssues = {
+                                        select: "ID,Title,Description,Modified,Function/Title, Item Owner/Title",
+                                        top: 500,
+                                        expand: "Function,Item Owner",
+                                        filter: "Risk Impact eq 'High '"
+                                    };
+        
+        vm.WorkPlans = [];
+        vm.listTitleWorkPlans = "Work Plan";
+        vm.paramsWorkPlans = {
+                                select: "ID,Task Owner/Title,Function/Title,Work Plan Task Type,Work Plan Task Status/Title,Due Date",
+                                top: 2000,
+                                orderby: "Due Date asc",
+                                expand: "Task Owner,Function,Work Plan Task Status"
+                            };
 
         activate();
 
         function activate() {
             $q.all([
-                getExerciseThreeData()
+                getRiskAndIssues(vm.listTitleRisksandIssues, vm.paramsHighRiskAndIssues),
+                getWorkplans(vm.listTitleWorkPlans, vm.paramsWorkPlans)
             ]).then(activateComplete);
 
             function activateComplete(results) {
-                vm.data = results[0];
+                vm.HighRiskAndIssues = results[0];
+                vm.WorkPlans = results[1];
             }
         }
 
-        function getExerciseThreeData() {
-            return restService.getExerciseThreeData().then(function(response) {
+        function getRiskAndIssues(listTitleRisksandIssues, paramsHighRiskAndIssues) {
+            return mockRestService.getListItems(listTitleRisksandIssues, paramsHighRiskAndIssues).then(function(response) {
                 return response;
             });
         }
 
-        //Add your code below.
-
-
+        function getWorkplans(listTitleWorkPlans, paramsWorkPlans) {
+            return mockRestService.getListItems(listTitleWorkPlans ,paramsWorkPlans).then(function(response) {
+                return response;
+            });
+        }
     }
 })();
